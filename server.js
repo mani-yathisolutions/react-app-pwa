0149bd5b00a2f1076ac532061e58311663989697
MNambaru <mnambaru@caci.co.uk>
2021-03-13 06:08:05 +0000

    Modified server.js

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
-const app = express();
-const PORT = process.env.PORT || 5000;
-
+let app = express();
 app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('*', function (req, res) {
- res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-app.listen(PORT);
+const port = process.env.PORT || '8080';
+app.set('port', port);
+const server = http.createServer(app);
+server.listen(port, () => console.log(`Running on :${port}`));

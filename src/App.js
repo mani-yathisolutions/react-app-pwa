9f4e62e1751b58456f6613db6a668efd127e8a7a
MNambaru <mnambaru@caci.co.uk>
2021-03-13 06:38:20 +0000

    Added QR code logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React from 'react';
+import React, { useState, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import QrReader from 'react-qr-scanner'
+
+const previewStyle = {
+  height: 240,
+  width: '100%',
+  marginTop: '40px',
+}
 
 function App() {
+  const [ showQrReader, setQrReader ] = useState(false);
+  const [qrResult, setQrResult] = useState(); 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      {!showQrReader ? (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <button onClick={() => setQrReader(true)}>Camera</button>
+          </header>
+        </div>
+      ) : (
+        <div>
+          <button style={{ marginTop: '20px', marginLeft: '20px' }} onClick={() => setQrReader(false)}>Back</button>
+          <QrReader
+            delay={300}
+            legacyMode={true}
+            style={previewStyle}
+            onError={(err) => console.log(err)}
+            onScan={(scan) => {
+              if(scan && qrResult !== scan) setQrResult(scan.text);
+            }}
+          />
+          <p>{qrResult}</p>
+        </div>
+      )}
+    </Fragment>
   );
 }
 
